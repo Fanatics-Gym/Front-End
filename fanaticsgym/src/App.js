860ea113fed24a0ba48f57241fc6a83fa66fb9cb
jarrodskahill <jarrodskahill@gmail.com>
2020-03-14 15:28:52 -0700

    reduced the size of the image and changed navbar

--- fanaticsgym/src/App.js
+++ fanaticsgym/src/App.js
@@ -5,11 +5,11 @@ import Header from "./Components/home/Header";
 import Footer from "./Components/nav/Footer";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./Styles/styles.scss";
+import "./Styles/app-styles.scss";
 import LoginForm from "./Components/login";
 import aboutUs from "./Components/about/about";
 import PrivateRoute from "./Components/Auth/PrivateRoute";
 import userPage from "./Components/user/userPage";
-
 import { initGA, PageView } from "./analyicts";
 
 function App() {
@@ -22,11 +22,13 @@ function App() {
     <div className="App">
       <Router>
         <NavBar />
-        <PrivateRoute path="/user" component={userPage} />
-        <Route path="/home" component={Header} />
-        <Route path="/login" component={LoginForm} />
-        <Route path="/about" component={aboutUs} />
-        <Footer className="foot" />
+        <div className="spacing">
+          <PrivateRoute path="/user" component={userPage} />
+          <Route path="/home" component={Header} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/about" component={aboutUs} />
+          {/* <Footer className="foot" /> */}
+        </div>
       </Router>
     </div>
   );
