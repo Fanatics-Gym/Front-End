bde23b4c2c23feebef561c85d39317706ad7315d
jarrodskahill <jarrodskahill@gmail.com>
2020-07-23 09:10:47 -0700

    changes to shop, moving add items to user page

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -2,23 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getItems } from "../Redux/thunks/itemThunk";
 import Item from "./item";
-import AddItem from "./addItem";
 import "../../scss/shop.scss";
 const Shop = () => {
-  // useEffect(() => {
-  //   BaseUrl()
-  //     .get("items")
-  //     .then((res) => {
-  //       console.log(res.data);
-  //       setShop(res.data);
-  //     });
-  // }, []);
-  const token = localStorage.getItem("token");
-  const userAdd = () => {
-    if (token) {
-      return <AddItem />;
-    }
-  };
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getItems());
@@ -27,7 +12,6 @@ const Shop = () => {
   return (
     <div>
       <h2 className="shopHeader">Shop</h2>
-      {userAdd()}
       <h3 className="itemsHeader">Items</h3>
       <div className="itemList">
         {itemsArray.itemArr.map((item) => (
--- src/Components/user/userPage.js
+++ src/Components/user/userPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../../scss/userPage.scss";
+import { Link } from "react-router-dom";
 const userPage = () => {
   return (
     <div className="userStyles">
       <h1>You've successfully logged in!!!</h1>
+      <Link to="/addItems">Add items</Link>
     </div>
   );
 };
