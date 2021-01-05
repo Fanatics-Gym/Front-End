18e39620902916f5d9baf42a77c5cf44e8f27b52
jarrodskahill <jarrodskahill@gmail.com>
2021-01-05 10:02:11 -0600

    finishing styling for item page

--- src/Components/Shop/itemPage.js
+++ src/Components/Shop/itemPage.js
@@ -5,11 +5,20 @@ import "../../scss/singleItem.scss";
 
 const ItemPage = () => {
   const Item = useRecoilValue(SelectedItem);
-  console.log(Item);
   return (
     <div className="singleItem">
       <div className="header">
+        <img src={Item.img} />
         <h1>{Item.name}</h1>
+      </div>
+      <div className="info">
+        <div className="describe">
+          <h3>Description</h3>
+          <p>
+            This is the new hat the best hat, it's so clean and so awesome! Made
+            with the best materials, silk, cotton
+          </p>
+        </div>
         <h2>${Item.price}</h2>
       </div>
     </div>
