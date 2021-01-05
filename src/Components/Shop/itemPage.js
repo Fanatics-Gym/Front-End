307463450d954a001119bfb6f8a3cbe0ed330ba3
jarrodskahill <jarrodskahill@gmail.com>
2021-01-05 14:58:23 -0600

    added checkout button to Itempage

--- src/Components/Shop/itemPage.js
+++ src/Components/Shop/itemPage.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useRecoilState } from "recoil";
 import SelectedItem from "../../Recoil/atom/selectedItem";
+import CheckoutAtom from "../../Recoil/atom/checkout";
 import "../../scss/singleItem.scss";
 
 const ItemPage = () => {
   const Item = useRecoilValue(SelectedItem);
+  const [checkout, setCheckout] = useRecoilState(CheckoutAtom);
+
+  const addToCheckout = () => {
+    setCheckout((old) => [...old, Item]);
+  };
   return (
     <div className="singleItem">
       <div className="header">
         <img src={Item.img} />
-        <h1>{Item.name}</h1>
+        <div>
+          <h1>{Item.name}</h1>
+          <h2>${Item.price}</h2>
+        </div>
       </div>
       <div className="info">
         <div className="describe">
@@ -19,7 +28,9 @@ const ItemPage = () => {
             with the best materials, silk, cotton
           </p>
         </div>
-        <h2>${Item.price}</h2>
+        <button className="item_btn" onClick={addToCheckout}>
+          Checkout
+        </button>
       </div>
     </div>
   );
