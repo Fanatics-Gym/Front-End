273fa2ead810535e3328a446f999a3dbc96ea799
jarrodskahill <jarrodskahill@gmail.com>
2021-01-06 15:00:48 -0600

    finishing up the display checkout information box

--- src/Components/Shop/checkout.js
+++ src/Components/Shop/checkout.js
@@ -10,12 +10,33 @@ const Checkout = () => {
       <div className="checkoutHeader">
         <h2>Checkout</h2>
       </div>
-      <div className="checkoutItems">
-        {checkoutList.map((item) => (
-          <Item key={item.id} item={item} />
-        ))}
+      <div className="checkoutCont">
+        <div className="checkoutItems">
+          {checkoutList.map((item) => (
+            <Item key={item.id} item={item} />
+          ))}
+        </div>
+        <div className="checkoutInfoCont">
+          <div className="checkoutInfo">
+            <p>
+              Number of Items:
+              <b>1</b>
+            </p>
+            <p>
+              SubTotal: <b>14</b>
+            </p>
+            <p>
+              Estimated Sales Tax:<b>0</b>
+            </p>
+            <p>
+              Total:<b>14</b>
+            </p>
+          </div>
+          <span>
+            <button className="purchase">Purchase</button>
+          </span>
+        </div>
       </div>
-      <div className="checkoutInfo"></div>
     </div>
   );
 };
