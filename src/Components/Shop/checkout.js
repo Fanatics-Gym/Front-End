804e06927fddae178d39ea2f196339f0d82dc512
jarrodskahill <jarrodskahill@gmail.com>
2021-01-14 22:34:33 -0600

    implementing stripe api

--- src/Components/Shop/checkout.js
+++ src/Components/Shop/checkout.js
@@ -2,13 +2,22 @@ import React from "react";
 import { useRecoilValue } from "recoil";
 import CheckoutAtom from "../../Recoil/atom/checkout";
 import Item from "./item";
+import { loadStripe } from "@stripe/stripe-js";
 
 const Checkout = () => {
+  const stripePromise = loadStripe(`${process.env.STRIPE_KEY}`);
+  console.log(process.env.STRIPE_KEY);
   const checkoutList = useRecoilValue(CheckoutAtom);
   const total = checkoutList
     .map((item) => item.price)
     .reduce((a, b) => a + b, 0);
-  console.log(total);
+
+  const handleClick = async (event) => {
+    const stripe = await stripePromise;
+    const response = await fetch("/create-checkout-session", {
+      method: "POST",
+    });
+  };
   return (
     <div className="checkout">
       <div className="checkoutHeader">
@@ -33,11 +42,13 @@ const Checkout = () => {
               Estimated Sales Tax:<b>0</b>
             </p>
             <p>
-              Total:<b>14</b>
+              Total:<b>${total}</b>
             </p>
           </div>
           <span>
-            <button className="purchase">Purchase</button>
+            <button className="purchase" onClick={handleClick}>
+              Purchase
+            </button>
           </span>
         </div>
       </div>
