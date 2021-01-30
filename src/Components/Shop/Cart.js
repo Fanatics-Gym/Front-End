c18799fd8ceda6db299b56525a64e185f133b89e
jarrodskahill <jarrodskahill@gmail.com>
2021-01-30 02:06:57 -0600

    preparing to implement stripe

--- src/Components/Shop/Cart.js
+++ src/Components/Shop/Cart.js
@@ -6,20 +6,41 @@ import Item from "./item";
 
 const Cart = () => {
   const checkoutList = useRecoilValue(CheckoutAtom);
+  const total = checkoutList
+    .map((item) => item.price)
+    .reduce((a, b) => a + b, 0);
   return (
     <div className="cart">
       <div className="cartHeader">
         <h2 className="cartTitle">Cart</h2>
         <span>
-          <Link to="/checkout">
-            <button className="toCheckout">Proceed To Checkout</button>
-          </Link>
+          <Link to="/checkout"></Link>
         </span>
       </div>
-      <div className="itemList">
-        {checkoutList.map((item) => (
-          <Item key={item.id} item={item} />
-        ))}
+      <div className="checkout">
+        <div className="checkoutCont">
+          <div className="checkoutItems">
+            {checkoutList.map((item) => (
+              <Item key={item.id} item={item} />
+            ))}
+          </div>
+          <div className="checkoutInfoCont">
+            <div className="checkoutInfo">
+              <p>
+                Number of Items:
+                <b>{checkoutList.length}</b>
+              </p>
+              <p>
+                SubTotal: <b>${total}</b>
+              </p>
+            </div>
+            <span>
+              <Link to="/checkout">
+                <button className="toCheckout">Checkout</button>
+              </Link>
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Components/Shop/checkout.js
+++ src/Components/Shop/checkout.js
@@ -3,21 +3,12 @@ import { useRecoilValue } from "recoil";
 import CheckoutAtom from "../../Recoil/atom/checkout";
 import Item from "./item";
 import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { BaseUrl } from "../Auth/axios";
 
 const Checkout = () => {
-  const stripePromise = loadStripe(`${process.env.STRIPE_KEY}`);
-  console.log(process.env.STRIPE_KEY);
   const checkoutList = useRecoilValue(CheckoutAtom);
-  const total = checkoutList
-    .map((item) => item.price)
-    .reduce((a, b) => a + b, 0);
 
-  const handleClick = async (event) => {
-    const stripe = await stripePromise;
-    const response = await fetch("/create-checkout-session", {
-      method: "POST",
-    });
-  };
   return (
     <div className="checkout">
       <div className="checkoutHeader">
@@ -28,28 +19,6 @@ const Checkout = () => {
           {checkoutList.map((item) => (
             <Item key={item.id} item={item} />
           ))}
-        </div>
-        <div className="checkoutInfoCont">
-          <div className="checkoutInfo">
-            <p>
-              Number of Items:
-              <b>{checkoutList.length}</b>
-            </p>
-            <p>
-              SubTotal: <b>${total}</b>
-            </p>
-            <p>
-              Estimated Sales Tax:<b>0</b>
-            </p>
-            <p>
-              Total:<b>${total}</b>
-            </p>
-          </div>
-          <span>
-            <button className="purchase" onClick={handleClick}>
-              Purchase
-            </button>
-          </span>
         </div>
       </div>
     </div>
