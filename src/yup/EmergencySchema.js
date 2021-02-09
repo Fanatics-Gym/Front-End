554af9f39680926fae7171b510d013c6ea91cc71
jarrodskahill <jarrodskahill@gmail.com>
2021-02-08 22:35:43 -0600

    emergency schema with yup is finished

---
+++ src/yup/EmergencySchema.js
@@ -0,0 +1,10 @@
+import * as Yup from "yup";
+
+export const EmergencySchema = Yup.object.shape({
+  Em_First: Yup.string().required("First name required"),
+  Em_Last: Yup.string().required("Last name required"),
+  relation: Yup.string().required("relation required"),
+  em_phone: Yup.string()
+    .matches(/^\d{10}$/, "Invalid phone number")
+    .required("Phone number is required"),
+});
