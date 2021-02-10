970c274ae2ada31184136ce42e247dfdfe761bdc
jarrodskahill <jarrodskahill@gmail.com>
2021-02-09 20:58:32 -0600

    finished confirmation page

--- src/Components/application/applicationForm/ConfirmationForm.js
+++ src/Components/application/applicationForm/ConfirmationForm.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const ConfirmationPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Thanks for applying</h2>
+      <h3>
+        We will reach out to you shortly regarding your application. We will
+        send you a message regarding wether you were approved or not. It should
+        take a few days.
+      </h3>
+      <p>If you have any questions feel free to contact this email</p>
+    </div>
+  );
 };
 
 export default ConfirmationPage;
