a0f8268552a1124509749ad82dfb5725d3334341
jarrodskahill <jarrodskahill@gmail.com>
2021-02-25 19:07:31 -0600

    removed dl_state from verfication schema

--- src/yup/verificationSchema.js
+++ src/yup/verificationSchema.js
@@ -2,7 +2,6 @@ import * as Yup from "yup";
 
 export const VerificationSchema = Yup.object().shape({
   Drivers_license: Yup.string().required("Drivers License is required"),
-  DL_state: Yup.string().required("Required"),
   DL_Expiration: Yup.string().required("Expiration required"),
   address: Yup.string().required("Address is required"),
   city: Yup.string().required("City is required"),
