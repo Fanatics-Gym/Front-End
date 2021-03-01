70af9cd6db29db7b948fc334eac4caebf8f1eeef
jarrodskahill <jarrodskahill@gmail.com>
2021-03-01 03:21:34 -0600

    created use effect that gets all applications and added component to app

--- src/Components/user/userViewApplications.js
+++ src/Components/user/userViewApplications.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import ApplicationAtom from "../../Recoil/atom/applicationsAtom";
+import { BaseUrl } from "../Auth/axios";
 
 const ViewApplications = () => {
+  const [applicationList, setApplicationList] = useRecoilState(ApplicationAtom);
+  useEffect(() => {
+    BaseUrl()
+      .get("applications/")
+      .then((res) => {
+        setApplicationList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  console.log(applicationList);
+
   return (
     <div>
       <h2>Applications</h2>
     </div>
   );
 };
+
+export default ViewApplications;
