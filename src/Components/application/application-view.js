4fea1e08aa858b5d370e9942334a1127406204b0
jarrodskahill <jarrodskahill@gmail.com>
2021-03-09 18:18:20 -0600

    capitalized status

--- src/Components/application/application-view.js
+++ src/Components/application/application-view.js
@@ -25,7 +25,7 @@ const ApplicationView = ({ application }) => {
       <p>Relation: {application.relation}</p>
       <p>Phone Number: {application.em_phone}</p>
       <h3>Application Status</h3>
-      <p>status: {application.status}</p>
+      <p>Status: {application.status}</p>
     </div>
   );
 };
