7eb4a2bc473b5e93a06f4c5d05c3fc7e13976fef
jarrodskahill <jarrodskahill@gmail.com>
2021-03-11 07:11:54 -0600

    added badges to the cart when its occupied

--- src/Components/nav/NavBar.js
+++ src/Components/nav/NavBar.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 import cart from "../../imgs/cart.png";
+import CheckoutSelector from "../../Recoil/selector/checkoutSelector";
 
 const NavBar = () => {
   const [navDrop, setNavDrop] = useState(false);
+  const { listLength } = useRecoilValue(CheckoutSelector);
   const adjust = () => {
     if (window.screen.width <= 500) {
       return <div></div>;
     }
   };
+  console.log(listLength);
   return (
     <nav className="nav">
       <div className="navLogo">
@@ -29,6 +33,13 @@ const NavBar = () => {
         </Link>
         <Link className="links" to="/cart">
           <img src={cart} />
+          {listLength !== 0 ? (
+            <span class="badge">
+              <h3 class="badgeNum">{listLength}</h3>
+            </span>
+          ) : (
+            <span></span>
+          )}
         </Link>
       </div>
     </nav>
