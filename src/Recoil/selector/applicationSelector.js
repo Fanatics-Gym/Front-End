7957727f988e3b5cc89685b9fbcdfad365172222
jarrodskahill <jarrodskahill@gmail.com>
2021-03-10 19:49:58 -0600

    created a selector for applications and some functions to determine status

---
+++ src/Recoil/selector/applicationSelector.js
@@ -0,0 +1,25 @@
+const { selector } = require("recoil");
+const { default: ApplicationAtom } = require("../atom/applicationsAtom");
+
+const ApplicationSelector = selector({
+  key: "applSelector",
+  get: ({ get }) => {
+    const applications = get(ApplicationAtom);
+    const totalAppl = applications.length;
+    const pendingAppl = applications.filter((appl) => appl.status === "Pending")
+      .length;
+    const approvedAppl = applications.filter(
+      (appl) => appl.status === "Approved"
+    );
+    const rejectedAppl = applications.filter(
+      (appl) => appl.status === "Rejected"
+    ).length;
+
+    return {
+      totalAppl,
+      pendingAppl,
+      approvedAppl,
+      rejectedAppl,
+    };
+  },
+});
