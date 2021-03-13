bbefe38bcc7108f17c5b234aaa9c13515afe72c8
jarrodskahill <jarrodskahill@gmail.com>
2021-03-13 06:11:08 -0600

    created a players stats section

--- src/Components/player/playerProfile.js
+++ src/Components/player/playerProfile.js
@@ -3,7 +3,7 @@ import { profileData, profileStats } from "./playerData";
 
 const PlayerProfile = () => {
   return (
-    <div className="playerCont">
+    <div className="componentCont">
       <div className="headerCont">
         <h2>Player Profile</h2>
       </div>
@@ -11,17 +11,35 @@ const PlayerProfile = () => {
         <section className="playerInfoSection">
           <div>
             <h3>Player Contact</h3>
-            <p>{profileData.userName}</p>
-            <p>{profileData.email}</p>
+            <p>
+              <b>UserName: </b>
+              {profileData.userName}
+            </p>
+            <p>
+              <b>Email: </b>
+              {profileData.email}
+            </p>
           </div>
         </section>
         <section className="playerInfoSection">
           <div>
             <h3>Player Stats </h3>
-            <p>{profileStats.tackles}</p>
-            <p>{profileStats.interception}</p>
-            <p>{profileStats.catches}</p>
-            <p>{profileStats.touchdowns}</p>
+            <p>
+              <b>Tackles: </b>
+              {profileStats.tackles}
+            </p>
+            <p>
+              <b>Interceptions: </b>
+              {profileStats.interception}
+            </p>
+            <p>
+              <b>Catches: </b>
+              {profileStats.catches}
+            </p>
+            <p>
+              <b>Touchdownes: </b>
+              {profileStats.touchdowns}
+            </p>
           </div>
         </section>
       </div>
