f2dc4d11330d3a3c168a817007a3efa98ac3f39a
jarrodskahill <jarrodskahill@gmail.com>
2021-03-13 05:02:59 -0600

    created player profile component

---
+++ src/Components/player/playerProfile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PlayerProfile = () => {
+  return (
+    <div>
+      <div>
+        <h2>Player Profile</h2>
+      </div>
+    </div>
+  );
+};
+
+export default PlayerProfile;
