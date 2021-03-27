2084e5454222f372e4f36a4aac84f7e27cbea0c3
Jarrod Skahill <jarrodskahill@gmail.com>
2021-03-27 03:09:24 -0700

    created more radio inputs and added some styling

--- src/Components/player/playerPickGear.js
+++ src/Components/player/playerPickGear.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 
 const PickGear = () => {
-    return <div> <div>
-        <h1>Pick Gear</h1>
-    </div>
+    return <div className='pickGearPage'> 
+        <div className='pickGearHeader'>
+            <h2>Pick Gear</h2>
+        </div>
+        <div className='pickGearContainer'>
         <div>                
             <h3>Helmet</h3>
             <span>
@@ -38,7 +40,30 @@ const PickGear = () => {
                 <label for='XL'>XL</label>
             </span>
         </div>
-        
+        <div>
+            <h3>Pants</h3>
+            <span>                
+                <input type='radio' name='Pants' value='M' />
+                <label for='M'>M</label>
+                <input type='radio' name='Pants' value='L' />
+                <label for='L'>L</label>
+                <input type='radio' name='Pants' value='XL'/>
+                <label for='XL'>XL</label>
+            </span>
+        </div>
+        <div>
+            <h3>Back Plate - Do you want one?</h3>
+            <span>
+                <input type='radio' name='backPlate' value='Yes' />
+                <label for='Yes'>Yes</label>
+                <input type='radio' name='backPlate' value='No' />
+                <label for='No'>No</label>
+            </span>
+        </div>
+        <div>
+            <h3>Pick Up</h3>
+        </div>
+        </div>
     </div>
 }
 
