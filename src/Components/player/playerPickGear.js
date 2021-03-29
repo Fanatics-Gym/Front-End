285f951062e71f2dade810d9867ad8b81838c200
jarrodskahill <jarrodskahill@gmail.com>
2021-03-29 06:13:02 -0500

    made labels go under radio inputs

--- src/Components/player/playerPickGear.js
+++ src/Components/player/playerPickGear.js
@@ -18,54 +18,82 @@ const PickGear = () => {
         <div className="pickGearInputs">
           <h3>Helmet: </h3>
           <span>
-            <input type="radio" name="helmet" value="M" />
-            <label for="M">M</label>
-            <input type="radio" name="helmet" value="L" />
-            <label for="L">L</label>
-            <input type="radio" name="helmet" value="XL" />
-            <label for="XL">XL</label>
+            <div>
+              <input type="radio" name="helmet" value="M" />
+              <label for="M">M</label>
+            </div>
+            <div>
+              <input type="radio" name="helmet" value="L" />
+              <label for="L">L</label>
+            </div>
+            <div>
+              <input type="radio" name="helmet" value="XL" />
+              <label for="XL">XL</label>
+            </div>
           </span>
         </div>
         <div className="pickGearInputs">
           <h3>Shoulder Pads: </h3>
           <span>
-            <input type="radio" name="Shoulder" value="M" />
-            <label for="M">M</label>
-            <input type="radio" name="Shoulder" value="L" />
-            <label for="L">L</label>
-            <input type="radio" name="Shoulder" value="XL" />
-            <label for="XL">XL</label>
+            <div>
+              <input type="radio" name="Shoulder" value="M" />
+              <label for="M">M</label>
+            </div>
+            <div>
+              <input type="radio" name="Shoulder" value="L" />
+              <label for="L">L</label>
+            </div>
+            <div>
+              <input type="radio" name="Shoulder" value="XL" />
+              <label for="XL">XL</label>
+            </div>
           </span>
         </div>
         <div className="pickGearInputs">
           <h3>Jeresy: </h3>
           <span>
-            <input type="radio" name="Jeresy" value="M" />
-            <label for="M">M</label>
-            <input type="radio" name="Jeresy" value="L" />
-            <label for="L">L</label>
-            <input type="radio" name="Jeresy" value="XL" />
-            <label for="XL">XL</label>
+            <div>
+              <input type="radio" name="Jeresy" value="M" />
+              <label for="M">M</label>
+            </div>
+            <div>
+              <input type="radio" name="Jeresy" value="L" />
+              <label for="L">L</label>{" "}
+            </div>
+            <div>
+              <input type="radio" name="Jeresy" value="XL" />
+              <label for="XL">XL</label>
+            </div>
           </span>
         </div>
         <div className="pickGearInputs">
           <h3>Pants: </h3>
           <span>
-            <input type="radio" name="Pants" value="M" />
-            <label for="M">M</label>
-            <input type="radio" name="Pants" value="L" />
-            <label for="L">L</label>
-            <input type="radio" name="Pants" value="XL" />
-            <label for="XL">XL</label>
+            <div>
+              <input type="radio" name="Pants" value="M" />
+              <label for="M">M</label>
+            </div>
+            <div>
+              <input type="radio" name="Pants" value="L" />
+              <label for="L">L</label>
+            </div>
+            <div>
+              <input type="radio" name="Pants" value="XL" />
+              <label for="XL">XL</label>
+            </div>
           </span>
         </div>
         <div className="pickGearInputs">
           <h3>Back Plate - Do you want one?: </h3>
           <span>
-            <input type="radio" name="backPlate" value="Yes" />
-            <label for="Yes">Yes</label>
-            <input type="radio" name="backPlate" value="No" />
-            <label for="No">No</label>
+            <div>
+              <input type="radio" name="backPlate" value="Yes" />
+              <label for="Yes">Yes</label>
+            </div>
+            <div>
+              <input type="radio" name="backPlate" value="No" />
+              <label for="No">No</label>
+            </div>
           </span>
         </div>
         <div className="pickGearInputs">
