8b617e850296d64708712783c7b7fed9af996e12
jarrodskahill <jarrodskahill@gmail.com>
2021-04-03 23:27:19 -0500

    created a cross app header div and h2, also a sign up page

---
+++ src/Components/login/signUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div>
+      <div className="pageHeader">
+        <h2>Sign Up</h2>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
