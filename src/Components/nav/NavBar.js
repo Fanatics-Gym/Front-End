aab54f88de9e244e1f9174377ac1e317200dfeb3
jarrodskahill <jarrodskahill@gmail.com>
2021-04-04 00:25:54 -0500

    updated nav to respond to userType

--- src/Components/nav/NavBar.js
+++ src/Components/nav/NavBar.js
@@ -2,17 +2,58 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import cart from "../../imgs/cart.png";
+import UserInfo from "../../Recoil/atom/userData";
 import CheckoutSelector from "../../Recoil/selector/checkoutSelector";
 
 const NavBar = () => {
   const [navDrop, setNavDrop] = useState(false);
   const { listLength } = useRecoilValue(CheckoutSelector);
+  const userInfo = useRecoilValue(UserInfo);
   const adjust = () => {
     if (window.screen.width <= 500) {
       return <div></div>;
     }
   };
-  const userDiv = () => {};
+  const userDiv = () => {
+    if (userInfo.user) {
+      if (userInfo.user.userType === "Admin") {
+        return (
+          <div className="navLinks">
+            <Link className="links" to="/">
+              Home
+            </Link>
+          </div>
+        );
+      } else if (userInfo.user.userType === "Player") {
+        console.log("Player");
+      }
+    } else {
+      return (
+        <div className="navLinks">
+          <Link className="links" to="/">
+            Home
+          </Link>
+          {/* <Link className="links" to="/shop">
+      Shop
+    </Link> */}
+          <Link className="links" to="/football">
+            Football
+          </Link>
+          {/* <Link className="links" to="/cart">
+      <img src={cart} />
+      {listLength !== 0 ? (
+        <span class="badge">
+          <h3 class="badgeNum">{listLength}</h3>
+        </span>
+      ) : (
+        <span></span>
+      )}
+    </Link> */}
+        </div>
+      );
+    }
+  };
+  userDiv();
   console.log(listLength);
   return (
     <nav className="nav">
@@ -21,27 +62,7 @@ const NavBar = () => {
           <h1>Fanatics Gym</h1>
         </Link>
       </div>
-      <div className="navLinks">
-        <Link className="links" to="/">
-          Home
-        </Link>
-        {/* <Link className="links" to="/shop">
-          Shop
-        </Link> */}
-        <Link className="links" to="/football">
-          Football
-        </Link>
-        {/* <Link className="links" to="/cart">
-          <img src={cart} />
-          {listLength !== 0 ? (
-            <span class="badge">
-              <h3 class="badgeNum">{listLength}</h3>
-            </span>
-          ) : (
-            <span></span>
-          )}
-        </Link> */}
-      </div>
+      {userDiv()}
     </nav>
   );
 };
