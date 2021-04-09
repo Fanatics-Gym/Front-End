fa48925925f30ea11ea650e068403db6800b98aa
jarrodskahill <jarrodskahill@gmail.com>
2021-04-08 22:13:48 -0500

    created links in player page and added it as a route to the app

--- src/Components/user/playerPage.js
+++ src/Components/user/playerPage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PlayerPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Link to="/player-profile">Profile</Link>
+      <Link to="/pick-gear">Pick Gear</Link>
+    </div>
+  );
 };
 
 export default PlayerPage;
