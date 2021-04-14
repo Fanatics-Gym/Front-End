4664388b9a52110f49728ea6381bbcad11b40d6a
jarrodskahill <jarrodskahill@gmail.com>
2021-04-13 19:47:50 -0500

    removed logout on admin page

--- src/Components/user/adminPage.js
+++ src/Components/user/adminPage.js
@@ -17,9 +17,6 @@ const AdminPage = (props) => {
       <h1>You've successfully logged in!!!</h1>
       <Link to="/add">Add items</Link>
       <Link to="/view-applications">View all applicants</Link>
-      <button className="logoutButton" onClick={logout}>
-        Logout
-      </button>
     </div>
   );
 };
