8e4abd9a84e0aad2b1ff89fd15182ea5c788609e
jarrodskahill <jarrodskahill@gmail.com>
2021-04-17 04:30:29 -0500

    player roster cards now have some style

--- src/Components/player/playerRosterCard.js
+++ src/Components/player/playerRosterCard.js
@@ -3,8 +3,10 @@ import React from "react";
 const RosterCard = ({ stats }) => {
   console.log(stats);
   return (
-    <div>
-      <p>UserName: {stats.username}</p>
+    <div className="rosterCard">
+      <div className="cardName">
+        <p> {stats.username}</p>
+      </div>
       <p>Touchdowns: {stats.touchdowns}</p>
       <p>Fumbles: {stats.fumbles}</p>
       <p>Interceptions: {stats.Interceptions}</p>
