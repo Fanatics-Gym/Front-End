21314d4856eee3402b51659167870671b5f99c26
jarrodskahill <jarrodskahill@gmail.com>
2021-04-17 03:05:02 -0500

    created a roster component to contain all the players

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ import PlayerProfile from "./Components/player/playerProfile";
 import PickGear from "./Components/player/playerPickGear";
 import SignUp from "./Components/login/signUp";
 import PlayerPage from "./Components/player/playerPage";
+import Roster from "./Components/player/playerRoster";
 
 // const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 function App() {
@@ -57,6 +58,7 @@ function App() {
           <Route path="/pick-gear" component={PickGear} />
           <Route path="/new-p-signUp/:id" component={SignUp} />
           <Route path="/player" component={PlayerPage} />
+          <Route path="/playerRoster" component={Roster} />
         </div>
       </Router>
     </div>
---
+++ src/Components/player/playerRoster.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Roster = () => {
+  return (
+    <div>
+      <div className="pageHeader">
+        <h2>Roster</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Roster;
