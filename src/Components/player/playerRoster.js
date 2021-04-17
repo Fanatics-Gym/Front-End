aedb8a8c8d09b3cf0195006b6d7193a8b92e7590
jarrodskahill <jarrodskahill@gmail.com>
2021-04-17 04:01:41 -0500

    created an atom for player roster and cards for each player on the roster

--- src/Components/player/playerRoster.js
+++ src/Components/player/playerRoster.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import PlayerRoster from "../../Recoil/atom/playerRosterAtom";
+import { BaseUrl } from "../Auth/axios";
+import RosterCard from "./playerRosterCard";
 
 const Roster = () => {
+  const [roster, setRoster] = useRecoilState(PlayerRoster);
+
+  useEffect(() => {
+    BaseUrl()
+      .get(`${process.env.REACT_APP_API_URL}user/players`)
+      .then((res) => {
+        setRoster(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  console.log(roster);
   return (
-    <div>
+    <div className="componentCont">
       <div className="pageHeader">
         <h2>Roster</h2>
+      </div>
+      <div>
+        {roster.map((player) => (
+          <RosterCard key={player.id} stats={player} />
+        ))}
       </div>
     </div>
   );
