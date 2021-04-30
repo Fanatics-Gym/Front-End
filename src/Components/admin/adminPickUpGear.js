643cdc63c6b42bfe3fec1bf3a6f7c059abc6e606
jarrodskahill <jarrodskahill@gmail.com>
2021-04-29 21:58:58 -0500

    added some more content to set pick up gear date, preparing for styling

--- src/Components/admin/adminPickUpGear.js
+++ src/Components/admin/adminPickUpGear.js
@@ -6,7 +6,15 @@ const PickGearDate = () => {
       <div className="pageHeader">
         <h2>Pick Up Date</h2>
       </div>
-      <input id="pickUpDate" name="pickUpDate" type="date" />
+      <section className="pickUpGearCont">
+        <div>
+          <h3>Choose a date for players to pick up gear</h3>
+          <input id="pickUpDate" name="pickUpDate" type="date" />
+        </div>
+        <div>
+          <h3>Dates set to pick up gear</h3>
+        </div>
+      </section>
     </div>
   );
 };
