63f985a9378ffdeafd4e59e6a07500505ffe8150
jarrodskahill <jarrodskahill@gmail.com>
2021-04-29 21:51:09 -0500

    created a pick up date component for players gear

---
+++ src/Components/admin/adminPickUpGear.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PickGearDate = () => {
+  return (
+    <div className="componentCont">
+      <div className="pageHeader">
+        <h2>Pick Up Date</h2>
+      </div>
+      <input id="pickUpDate" name="pickUpDate" type="date" />
+    </div>
+  );
+};
+
+export default PickGearDate;
