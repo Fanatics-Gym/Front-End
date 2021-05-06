58f5759f0e1b3b6b00f17e1bf3d400ccd90a607b
jarrodskahill <jarrodskahill@gmail.com>
2021-05-05 22:18:04 -0500

    made request to backend with useeffect and created styling for the table

--- src/Components/admin/pickUpTable/pickUpTableDisplay.js
+++ src/Components/admin/pickUpTable/pickUpTableDisplay.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import PickUpDatesPlayers from "../../../Recoil/atom/pickUpDatesPlayersAtom";
 import PickUpTable from "./pickUpTable";
+import { BaseUrl } from "../../Auth/axios";
 
 function PickUpDisplay() {
+  const [playersGear, setPlayersGear] = useRecoilState(PickUpDatesPlayers);
+  useEffect(() => {
+    BaseUrl()
+      .get(`pickUp/playersPickUp`)
+      .then((res) => {
+        setPlayersGear(res.data);
+      });
+  }, []);
   const columns = React.useMemo(() => [
     {
       Header: "Name",
@@ -30,8 +41,8 @@ function PickUpDisplay() {
   ]);
 
   return (
-    <div>
-      <PickUpTable columns={columns} />
+    <div className="pickUpTable">
+      <PickUpTable columns={columns} data={playersGear} />
     </div>
   );
 }
