46380a49f4542cfa56a65fc6e8ac8484244cc68d
jarrodskahill <jarrodskahill@gmail.com>
2021-05-18 17:07:21 -0500

    added some content to rules page

--- src/Components/Rules/rules.js
+++ src/Components/Rules/rules.js
@@ -1,11 +1,31 @@
 import React from "react";
 import RulesHeader from "../../imgs/rules.png";
+import { rulesData } from "./rulesData";
 
 const Rules = () => {
   return (
     <div className="componentCont">
       <div className="rulesCont">
         <img className="rulesHeader" src={RulesHeader} />
+        <section className="intro">
+          <p>{rulesData[0]}</p>
+        </section>
+        <section className="rulesSection">
+          <h3>Kickoff</h3>
+          <ul>
+            {rulesData[1].map((li) => (
+              <li>{li}</li>
+            ))}
+          </ul>
+        </section>
+        <section className="rulesSection">
+          <h3>Onside Kick</h3>
+          <ul>
+            {rulesData[2].map((li) => (
+              <li>{li}</li>
+            ))}
+          </ul>
+        </section>
       </div>
     </div>
   );
---
+++ src/Components/Rules/rulesData.js
@@ -0,0 +1,18 @@
+export const rulesData = [
+  "We (Fanatics Football) play by a combination of the common and basic rules of football as practiced by the National Federation of State High School Associations (NFHS), National Collegiate Athletic Association (NCAA), and the National Football League (NFL). By participating, it is conclusively presumed that each participant understands and follows/will follow the basic, fundamental, rules of football. In addition to the previously mentioned, the following rules are recognized, practiced, and enforced by Fanatics Football:",
+  [
+    "Ball is placed/kickoff at 35-yard line.",
+    "Players may get a running start before kickoff, but must remain behind the Line of Scrimmage until the ball is kicked.",
+    "Ball is kicked into the Endzone/Fair Catch in the Endzone (Touchback), ball is placed at the 25 yard-line",
+    "Fair Catch outside of the Endzone – ball is placed at the Fair Catch spot.",
+    "Return Team may create a 'wedge' of up to 3 players/blockers.",
+    "Drop-Kick is allowed for Kickoff.",
+  ],
+  [
+    "Players may get a running start before Onside kick, but must remain behind the Line of Scrimmage until the ball is kicked.",
+    "Kicking Team may 'overload' one side of the ball up to 10 players on one side",
+    "Kicking Team may engage Receiving Team before the ball passes 10 yards",
+    "Kicking Team may engage Receiving Team before any player on the Receiving Team touches the ball.",
+    "Drop-Kick is allowed for Onside Kick.",
+  ],
+];
