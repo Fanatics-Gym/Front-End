846cd35e72b2e9c17b538d6df7c28ca83d306c52
jarrodskahill <jarrodskahill@gmail.com>
2021-05-18 16:22:30 -0500

    added the rule book photo with some styling

--- src/Components/Rules/rules.js
+++ src/Components/Rules/rules.js
@@ -1,7 +1,14 @@
 import React from "react";
+import RulesHeader from "../../imgs/rules.png";
 
 const Rules = () => {
-  return <div className="componentCont"></div>;
+  return (
+    <div className="componentCont">
+      <div className="rulesCont">
+        <img className="rulesHeader" src={RulesHeader} />
+      </div>
+    </div>
+  );
 };
 
 export default Rules;
