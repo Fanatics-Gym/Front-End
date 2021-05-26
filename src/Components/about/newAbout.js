01e5dd0bdcb4c7018b06c97f9db97d494dcff39f
jarrodskahill <jarrodskahill@gmail.com>
2021-05-25 20:47:09 -0500

    I made some changes to the about pages margin

--- src/Components/about/newAbout.js
+++ src/Components/about/newAbout.js
@@ -8,17 +8,17 @@ const NewAbout = () => {
       </div>
       <div className="aboutCont">
         <h3>
-          At Fanatics Football, we play <b>Football</b>. We are an 11-on-11,
-          8-Team league, where each team operates the way they want. Only want
-          to practice once a week? Great! Don’t want to practice at all?
-          Perfect! Just want to strap up on game day and get after it? 100%
-          Let’s Go! We are for anyone who misses the feeling of going out, under
-          the lights, and playing all out for 60 minutes. Our equipment is top
+          At Fanatics Football, we play <b>Football</b>.  We are an 11-on-11,
+          8-Team league, where each team operates the way they want.  Only want
+          to practice once a week?  Great!  Don’t want to practice at all? 
+          Perfect!  Just want to strap up on game day and get after it?  100%
+          Let’s Go!  We are for anyone who misses the feeling of going out, under
+          the lights, and playing all out for 60 minutes.  Our equipment is top
           notch; our uniforms, outstanding; and our customizable helmets allow
           our players the freedom of design and promotion of their
-          brand/business. We are a league that values the tradition of football
-          and keeps the integrity of the game. So, live the dream, strap up, and
-          joint us, today.
+          brand/business.  We are a league that values the tradition of football
+          and keeps the integrity of the game.  So, live the dream, strap up, and
+          join us, today.
         </h3>
       </div>
     </div>
