75c9c564a71b8cfa25bba85f507e7e27377f2c90
jarrodskahill <jarrodskahill@gmail.com>
2021-05-27 23:24:12 -0500

    created a new component

---
+++ src/Components/player/checkoutGear.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CheckoutGearPage = () => {
+  return (
+    <div className="componentCont">
+      <div className="pageHeader">
+        <h2>Checkout</h2>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutGearPage;
