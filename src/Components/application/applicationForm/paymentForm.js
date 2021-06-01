9cb6c3672d304a346d6540f9191db7624f11e304
jarrodskahill <jarrodskahill@gmail.com>
2021-05-31 21:02:37 -0500

    created the payment box with styles

--- src/Components/application/applicationForm/paymentForm.js
+++ src/Components/application/applicationForm/paymentForm.js
@@ -1,9 +1,29 @@
-import React from 'react';
-
+import React from "react";
 
 const PaymentForm = () => {
-    return <div></div>
-}
+  return (
+    <div className="paymentCont">
+      <h3>Payment</h3>
+      <div className="paymentInfoCont">
+        <div className="paymentInfo">
+          <h4>Season {"&"} Gear</h4>
+          <b>$750.00</b>
+        </div>
+        <div className="paymentInfo">
+          <h4>Security Deposit</h4>
+          <b>$750.00</b>
+        </div>
+        <div className="paymentInfo" id="total">
+          <h4>Total:</h4>
+          <b>$1500.00</b>
+        </div>
+      </div>
+      <div>
+        <input type="checkbox" />
+        <label></label>
+      </div>
+    </div>
+  );
+};
 
-
-export default PaymentForm
+export default PaymentForm;
