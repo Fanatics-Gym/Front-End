d8a892ebf827d716084f4a481ec8ac4a2c40b029
jarrodskahill <jarrodskahill@gmail.com>
2021-06-01 19:09:52 -0500

    added some content to the terms of cost page and some styling to the text

--- src/Components/application/applicationForm/termOfCost.js
+++ src/Components/application/applicationForm/termOfCost.js
@@ -3,8 +3,49 @@ import React from "react";
 const TermsOfCost = () => {
   return (
     <div className="componentCont">
-      <div className="pageHeader">
+      <div className="termsHeader">
         <h2>Terms of Cost</h2>
+      </div>
+      <div className="termsContent">
+        <div>
+          <ol type="1">
+            <li>
+              <ol type="A">
+                <li>
+                  &nbsp;<b>SEASON {"&"} GEAR:</b> &nbsp; The cost for “Season{" "}
+                  {"&"} Gear” includes the following: (i) Participation in an
+                  8-Game Season (includes Practice Game/Week 0 {"&"}{" "}
+                  Postseason/Playoffs – if your team makes it), (ii) helmet,
+                  shoulder pads, leg pads, uniform(s), back plate (<b>“Gear”</b>
+                  ).
+                </li>
+                <li>
+                  &nbsp;<b>SECURITY DEPOSIT: </b> &nbsp;(Participant) (You)
+                  “Security Deposit” (Security Deposit) will be held by Fanatics
+                  Football. All, or any portion of the Security Deposit, may be
+                  used, as reasonably necessary, to: (i) replace lost Gear of
+                  Participant and (ii) replace damaged (excluding ordinary
+                  damage/wear {"&"}
+                  tear) Gear of Participant.
+                </li>
+                <ol type="I">
+                  <li>
+                    If all or any amount of the Security Deposit is used during
+                    Participant’s participation, Participant agrees to reinstate
+                    the total amount of Security Deposit within fourteen (14)
+                    days after Fanatics Football gives Participant notice, via
+                    email. If the total amount of the Security Deposit is not
+                    reinstated within 14 days, Fanatics Football reserves the
+                    right to immediately terminate Participant from
+                    participating in, but not limited to, the following: any
+                    current and/or further activities, events, games, and
+                    functions of/put on by Fanatics Football.
+                  </li>
+                </ol>
+              </ol>
+            </li>
+          </ol>
+        </div>
       </div>
     </div>
   );
