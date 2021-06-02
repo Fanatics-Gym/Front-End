cc2af559d7427141cf36f4877349fb0c94923c1d
jarrodskahill <jarrodskahill@gmail.com>
2021-06-02 01:59:17 -0500

    removed emojis from links

--- src/Components/nav/menu/menu.js
+++ src/Components/nav/menu/menu.js
@@ -7,46 +7,21 @@ const Menu = ({ open }) => {
   return (
     <StyledMenu open={open}>
       <Link to="/about">
-        <a>
-          <span role="img" aria-label="about us">
-            &#x1f481;&#x1f3fb;&#x200d;&#x2642;&#xfe0f;
-          </span>
-          About us
-        </a>
+        <a>About us</a>
       </Link>
       <Link to="/teams">
-        <a>
-          <span role="img" aria-label="price">
-            &#x1f4b8;
-          </span>
-          Teams
-        </a>
+        <a>Teams</a>
       </Link>
       <Link to="/rules">
-        <a>
-          <span role="img" aria-label="contact">
-            &#128203;
-          </span>
-          Rules
-        </a>
+        <a>Rules</a>
       </Link>
       <Link to="/login">
         {" "}
-        <a>
-          <span role="img" aria-label="contact">
-            &#127944;
-          </span>
-          Login
-        </a>
+        <a>Login</a>
       </Link>
       <Link to="/contact">
         {" "}
-        <a>
-          <span role="img" aria-label="contact">
-            &#x1f4e9;
-          </span>
-          Contact
-        </a>
+        <a>Contact</a>
       </Link>
     </StyledMenu>
   );
