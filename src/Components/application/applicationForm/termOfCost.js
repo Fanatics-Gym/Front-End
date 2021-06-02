f17bb3c21615636bcd082bdb043cd9e83511b739
jarrodskahill <jarrodskahill@gmail.com>
2021-06-01 19:24:32 -0500

    added more content to the page

--- src/Components/application/applicationForm/termOfCost.js
+++ src/Components/application/applicationForm/termOfCost.js
@@ -20,7 +20,7 @@ const TermsOfCost = () => {
                   ).
                 </li>
                 <li>
-                  &nbsp;<b>SECURITY DEPOSIT: </b> &nbsp;(Participant) (You)
+                  &nbsp;<b>SECURITY DEPOSIT:</b> &nbsp; (Participant) (You)
                   “Security Deposit” (Security Deposit) will be held by Fanatics
                   Football. All, or any portion of the Security Deposit, may be
                   used, as reasonably necessary, to: (i) replace lost Gear of
@@ -30,16 +30,70 @@ const TermsOfCost = () => {
                 </li>
                 <ol type="I">
                   <li>
-                    If all or any amount of the Security Deposit is used during
-                    Participant’s participation, Participant agrees to reinstate
-                    the total amount of Security Deposit within fourteen (14)
-                    days after Fanatics Football gives Participant notice, via
-                    email. If the total amount of the Security Deposit is not
-                    reinstated within 14 days, Fanatics Football reserves the
-                    right to immediately terminate Participant from
+                    &nbsp; If all or any amount of the Security Deposit is used
+                    during Participant’s participation, Participant agrees to
+                    reinstate the total amount of Security Deposit within
+                    fourteen (14) days after Fanatics Football gives Participant
+                    notice, via email. If the total amount of the Security
+                    Deposit is not reinstated within 14 days, Fanatics Football
+                    reserves the right to immediately terminate Participant from
                     participating in, but not limited to, the following: any
                     current and/or further activities, events, games, and
                     functions of/put on by Fanatics Football.
+                  </li>
+                </ol>
+              </ol>
+            </li>
+            <li>
+              <ol type="A">
+                <li>
+                  &nbsp;<b>RETURN GEAR:</b> At the end of Participant’s last
+                  game, Participant agrees to return all Gear to Fanatics
+                  Football.
+                </li>
+                <ol type="I">
+                  <li>
+                    {" "}
+                    &nbsp; Participant agrees and understands that all Gear is
+                    to be returned to Fanatics Football within 14 days after the
+                    Participant’s last game.
+                  </li>
+                  <ol type="a">
+                    <li>
+                      &nbsp; If Participant’s Gear is not returned to Fanatics
+                      Football within 14 days, Participant agrees to pay a
+                      $100.00 Late Fee (<b>Late Fee</b>), starting on the 15th
+                      day, and Participant agrees to pay an additional $100.00
+                      Late Fee for each day that Participant’s Gear is not
+                      returned to Fanatics Football
+                    </li>
+                    <li>
+                      &nbsp; If Participant fails to return the Gear to Fanatics
+                      Football within 24 days, Participant agrees to pay
+                      $1,000.00 to Fanatics Football and forfeits all/any
+                      remaining amount of the Security Deposit to Fanatics
+                      Football.
+                    </li>{" "}
+                    <li>
+                      &nbsp; Participant agrees to and understands that the
+                      Security Deposit, or any amount of the Security Deposit,
+                      is not and will not be used/put toward the $1,000.00 that
+                      is specified in paragraph 2.A.(a) {"&"} (b).
+                    </li>
+                  </ol>
+                  <li>
+                    &nbsp; Within 21 days after all the Gear is returned to and
+                    received by Fanatics Football, Fanatics Football will email
+                    Participant an itemized statement of all lost and/or damaged
+                    Gear (if any) and the amount of the Security Deposit used
+                    (if any) to replace the item(s) of the Gear and return any
+                    remaining amount of the Security Deposit to the Participant.
+                  </li>
+                  <li>
+                    &nbsp; Please, note that any amount of the Security Deposit
+                    will not be returned to the Participant until after all
+                    items of the Gear are returned to and received by Fanatics
+                    Football and the/all Late Fees (if any) are paid.
                   </li>
                 </ol>
               </ol>
