c1185177c56f3cb1af9dd94ac774821c11d2eabe
jarrodskahill <jarrodskahill@gmail.com>
2021-07-08 13:01:00 -0500

    created a component for creating teams

--- src/Components/teams/createTeam.js
+++ src/Components/teams/createTeam.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const CreateTeam = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <label>Name</label>
+        <input />
+      </div>
+      <div>
+        <label>Img</label>
+        <input />
+      </div>
+      <button>Create</button>
+    </div>
+  );
 };
 
 export default CreateTeam;
